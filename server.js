004b16f39330b09ec280820d9f8ddb68ea0c7749
wescleysudre <wescley.sudre@sage.com>
2020-06-14 21:32:56 -0300

    Adicionando server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/naBoleiaApp'));
+
+app.get('/*', function (req, res) {
+
+  res.sendFile(path.join(__dirname + '/dist/naBoleiaApp/index.html'));
+});
+
+// Inicia a aplicação pela porta configurada
+app.listen(process.env.PORT || 8080);
