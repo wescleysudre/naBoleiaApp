4b6f40eafc48d1911f42b5b86f079476266caf99
wescleysudre <wescley.sudre@sage.com>
2020-06-14 21:43:14 -0300

    Variável de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+
+  mapbox: {
+    acces_token: 'pk.eyJ1Ijoid2VzY2xleXN1ZHJlIiwiYSI6ImNrYmZnb3VneTB2Y3cyeG52bmpnZXRzemYifQ.0XGIp-sLG6BHxVVc_XesLA'
+  }
 };
